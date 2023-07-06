be42875bcfac065d6e72aeeb45041578d4b129fe
RobertoCiurea <robertociureaionut@gmail.com>
2023-07-06 18:38:53 +0300

    added total of shopping cart

--- client/src/pages/Product/ProductPage.jsx
+++ client/src/pages/Product/ProductPage.jsx
@@ -26,41 +26,43 @@ export const ProductPage = ({ product }) => {
   return (
     <div className="container">
       <Navbar modalRef={modalRef} dialogRef={dialogRef} />
-      <ShoppingCart dialogRef={dialogRef} items={items} setItems={setItems} />
-      <Modal modalRef={modalRef} />
-      <section className="product-section">
-        <img src={images[color]} alt="Iphone 14 Pro max" />
-        <div className="product-content">
-          <h1>{product.title}</h1>
-          <div className="color-options">
-            <button
-              className="purple-button"
-              onClick={() => setColor(1)}
-            ></button>
-            <button
-              className="gold-button"
-              onClick={() => setColor(2)}
-            ></button>
-            <button
-              className="silver-button"
-              onClick={() => setColor(3)}
-            ></button>
-            <button
-              className="black-button"
-              onClick={() => setColor(0)}
-            ></button>
+      <main className="main-section">
+        <ShoppingCart dialogRef={dialogRef} items={items} setItems={setItems} />
+        <Modal modalRef={modalRef} />
+        <section className="product-section">
+          <img src={images[color]} alt="Iphone 14 Pro max" />
+          <div className="product-content">
+            <h1>{product.title}</h1>
+            <div className="color-options">
+              <button
+                className="purple-button"
+                onClick={() => setColor(1)}
+              ></button>
+              <button
+                className="gold-button"
+                onClick={() => setColor(2)}
+              ></button>
+              <button
+                className="silver-button"
+                onClick={() => setColor(3)}
+              ></button>
+              <button
+                className="black-button"
+                onClick={() => setColor(0)}
+              ></button>
+            </div>
+            <h3 className="cut-deal">
+              {" "}
+              <s>{product.cutPrice} Lei </s>
+            </h3>
+            <h2>{product.price} Lei</h2>
+            <button className="buy-button" onClick={() => addItems(product)}>
+              Adauga in cos
+            </button>
           </div>
-          <h3 className="cut-deal">
-            {" "}
-            <s>{product.cutPrice} Lei </s>
-          </h3>
-          <h2>{product.price} Lei</h2>
-          <button className="buy-button" onClick={() => addItems(product)}>
-            Adauga in cos
-          </button>
-        </div>
-      </section>
-      <Footer />
+        </section>
+        <Footer />
+      </main>
     </div>
   );
 };
